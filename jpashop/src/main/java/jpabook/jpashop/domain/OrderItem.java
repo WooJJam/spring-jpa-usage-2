13e70300b7a0677792796d78d407c0e27963bb35
WooJJam <jaemin5548@naver.com>
2024-01-28 02:02:48 +0900

    [Section 3] API 개발 고급 - 지연 로딩과 조회 성능 최적화 (간단한 주문 조회 V1: 엔티티를 직접 노출)
    Signed-off-by: WooJJam <jaemin5548@naver.com>

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
@@ -19,6 +20,7 @@ public class OrderItem {
     @Column(name = "orderItem_id")
     private Long id;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
